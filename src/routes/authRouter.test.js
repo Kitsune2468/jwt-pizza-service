7423b2b432a33b23f3772520917fde602f622936
Kitsune2468 <masefro@gmail.com>
2025-02-04 23:38:51 -0700

    Testing admin stuff

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../service');
+const { DB, Role } = require('../../src/database/database');
 
 const testName = 'pizza diner';
 const testEmail = 'reg@test.com';
@@ -7,28 +8,59 @@ const testPassword = 'a';
 const testUser = { name: testName, email: testEmail, password: testPassword };
 let testUserAuthToken;
 
+const testAdminName = 'test admin';
+const testAdminEmail = 'admin@test.com';
+const testAdminPassword = 'a';
+const testAdmin = { name: testAdminName, email: testAdminEmail, password: testAdminPassword };
+let testAdminAuthToken;
+
 function randomName() {
     return Math.random().toString(36).substring(2, 12);
 }
 
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+  
+    user = await DB.addUser(user);
+  
+    return { ...user, password: 'toomanysecrets' };
+}
+
 beforeAll(async () => {
-  testUser.email = randomName() + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
-  testUserAuthToken = registerRes.body.token;
-  expectValidJwt(testUserAuthToken);
+    testUser.email = randomName() + '@test.com';
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    expectValidJwt(testUserAuthToken);
+});
+
+test('create/login admin', async () => {
+    adminResult = createAdminUser();
+
+    const loginRes = await request(app).put('/api/auth').send(adminResult);
+    expect(loginRes.status).toBe(200);
+    expectValidJwt(loginRes.body.token);
+
+    const expectedUser = { ...testAdmin, roles: [{ role: 'Admin' }] };
+    delete expectedUser.password;
+    expect(loginRes.body.user).toMatchObject(expectedUser);
+
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testAdminAuthToken}`);
+    expect(logoutRes.status).toBe(200);
 });
 
 test('login & logout', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  expect(loginRes.status).toBe(200);
-  expectValidJwt(loginRes.body.token);
+    const loginRes = await request(app).put('/api/auth').send(testUser);
+    expect(loginRes.status).toBe(200);
+    expectValidJwt(loginRes.body.token);
 
-  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
-  delete expectedUser.password;
-  expect(loginRes.body.user).toMatchObject(expectedUser);
+    const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+    delete expectedUser.password;
+    expect(loginRes.body.user).toMatchObject(expectedUser);
 
-  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
-  expect(logoutRes.status).toBe(200);
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(logoutRes.status).toBe(200);
 });
 
 test('bad login', async() => {
--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../service');
+const { Role, DB } = require('../database/database.js');
 
 const testName = 'pizza diner';
 const testEmail = 'reg@test.com';
@@ -7,11 +8,15 @@ const testPassword = 'a';
 const testUser = { name: testName, email: testEmail, password: testPassword };
 let testUserAuthToken;
 
+const testAdminName = 'test admin';
+const testAdminEmail = 'admin@test.com';
+const testAdminPassword = 'a';
+const testAdmin = { name: testAdminName, email: testAdminEmail, password: testAdminPassword };
+let testAdminAuthToken;
+
 function randomName() {
   return Math.random().toString(36).substring(2, 12);
 }
-
-const { Role, DB } = require('../database/database.js');
 
 async function createAdminUser() {
   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
@@ -19,6 +24,7 @@ async function createAdminUser() {
   user.email = user.name + '@admin.com';
 
   user = await DB.addUser(user);
+
   return { ...user, password: 'toomanysecrets' };
 }
 
@@ -26,10 +32,19 @@ beforeAll(async () => {
   adminUser = createAdminUser();
   const loginAdminRes = await request(app).put('/api/auth').send(adminUser);
 
+  console.log(loginAdminRes);
   adminAuthToken = loginAdminRes.body.token;
+  adminEmail = adminUser.email;
 });
 
 test('get all franchises', async () => {
   franchiseRes = await request(app).get('/api/franchise');
   expect(franchiseRes.status).toBe(200);
 });
+/*
+test('create franchises', async () => {
+  console.log(adminEmail);
+  franchiseRes = await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminAuthToken}`).send({ name: 'TestFranchise', admins: [{ email: adminEmail }] });
+  expect(franchiseRes.status).toBe(200);
+});
+*/
