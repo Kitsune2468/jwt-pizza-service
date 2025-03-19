330f32bce8705df536d9968f7aad9f80a97aac13
Kitsune2468 <masefro@gmail.com>
2025-03-18 21:43:45 -0600

    Deliverable 8 - Started systemMetrics().

--- src/metrics.js
+++ src/metrics.js
@@ -3,28 +3,26 @@ const os = require('os');
 
 let metrics = [];
 
+// http Metrics
 let totalRequests = 0;
 let deleteRequests = 0;
 let getRequests = 0;
 let postRequests = 0;
 let putRequests = 0;
 
-let activeUsers = 0;
-
+// system Metrics
+let memoryUsage = 0;
+let cpuUsage = 0;
 let successfulAuth = 0;
 let failedAuth = 0;
+let requestLatency = 0;
+let pizzaLatency = 0;
 
-let totalMem = 0;
-let currentMem = 0;
-let freeMem = 0;
-let cpuUsage = 0;
-
+// stats Metrics
+let activeUsers = 0;
 let pizzasSold = 0;
 let pizzaFails = 0;
 let revenue = 0;
-
-let requestLatency = 0;
-let pizzaLatency = 0;
 
 function httpMetrics() {
     totalRequests = deleteRequests + getRequests + postRequests + putRequests;
@@ -35,49 +33,63 @@ function httpMetrics() {
     addMetric('deleteRequests', deleteRequests, 'sum', '1');
 }
 
-function addMetric(metricName, metricValue, type, unit) {
-    const metric = {
-        resourceMetrics: [
-          {
-            scopeMetrics: [
-              {
-                metrics: [
-                  {
-                    name: metricName,
-                    unit: unit,
-                    [type]: {
-                      dataPoints: [
-                        {
-                          asInt: metricValue,
-                          timeUnixNano: Date.now() * 1000000,
-                        },
-                      ],
-                    },
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      };
-    
-      if (type === 'sum') {
-        metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
-        metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].isMonotonic = true;
-      }
-      metrics.push(metric);
+function systemMetrics() {
+    memoryUsage = getMemoryUsagePercentage();
+    cpuUsage = getCpuUsagePercentage();
+    addMetric('memoryUsage', memoryUsage, 'gauge', '%');
+    addMetric('cpuUsage', cpuUsage, 'gauge', '%');
+
+    addMetric('successfulAuth', successfulAuth, 'sum', '1');
+    addMetric('failedAuth', failedAuth, 'sum', '1');
+
+    // TODO: Latency calcs
+    addMetric('requestLatency', requestLatency, 'histogram', 'ms');
 }
 
-setInterval(() => {
-  const cpuValue = Math.floor(Math.random() * 100) + 1;
-  sendMetricToGrafana('cpu', cpuValue, 'gauge', '%');
+function getCpuUsagePercentage() {
+  const cpuUsage = os.loadavg()[0] / os.cpus().length;
+  return cpuUsage.toFixed(2) * 100;
+}
 
-  requests += Math.floor(Math.random() * 200) + 1;
-  sendMetricToGrafana('requests', requests, 'sum', '1');
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
 
-  latency += Math.floor(Math.random() * 200) + 1;
-  sendMetricToGrafana('latency', latency, 'sum', 'ms');
-}, 1000);
+function addMetric(metricName, metricValue, type, unit) {
+    const metric = {
+        name: metricName,
+        unit: unit,
+        [type]: {
+            dataPoints: [
+                {
+                    asInt: metricValue,
+                    timeUnixNano: Date.now() * 1000000,
+                },
+            ],
+        },
+    };
+    
+    if (type === 'sum') {
+    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
+    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].isMonotonic = true;
+    }
+    metrics.push(metric);
+}
+
+function sendMetricsPeriodically(period) {
+    const timer = setInterval(() => {
+      try {
+        this.sendMetricToGrafana(metrics);
+        metrics = [];
+      } catch (error) {
+        console.log('Error sending metrics', error);
+      }
+    }, period);
+  }
 
 function sendMetricToGrafana(metricName, metricValue, type, unit) {
   const metric = {
