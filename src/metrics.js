95256925a38aa871b106f6822f25a3e68af782f4
Kitsune2468 <masefro@gmail.com>
2025-03-19 12:51:29 -0600

    Deliverable 8 - Updated metrics.js to not have double for memUsage

--- src/metrics.js
+++ src/metrics.js
@@ -69,8 +69,8 @@ function getMemoryUsagePercentage() {
   const totalMemory = os.totalmem();
   const freeMemory = os.freemem();
   const usedMemory = totalMemory - freeMemory;
-  const memoryUsage = (usedMemory / totalMemory) * 100;
-  return memoryUsage.toFixed(2);
+  const memoryUsage = (usedMemory / totalMemory);
+  return memoryUsage.toFixed(2) * 100;
 }
 
 function addMetric(metricName, metricValue, type, unit) {
